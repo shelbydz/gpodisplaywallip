03f4ee7a8aaedb6d842c02d50ba73e10d2a8dffe
shawn sullivan <shelbydz@gmail.com>
2018-04-25 10:25:42 -0400

    cleaning up code, OSD works

--- driver.lua
+++ driver.lua
@@ -10,34 +10,34 @@ SERIAL_BINDING = 1
 SET_ID = "00"
 
 -- Serial Commands
-OSD_MENU = "F4 88 %s %s"
+OSD_MENU = "F4 88 %s FD %s"
 KEY_CODES = {
-		["MENU"]	= "43",
-		["EXIT"]	= "50",
-		["DOWN"]	= "4B",
+	   ["MENU"]		= "43",
+		["CANCEL"]	= "50", -- Exit on the remote
+		["DOWN"]		= "4B",
 		["UP"]		= "47",
-		["LEFT"]	= "57",
-		["RIGHT"]	= "53",
-		["ENTER"]	= "04",
-		["NUMBER_1"]		= "1D",
-		["NUMBER_2"]		= "1C",
-		["NUMBER_3"]		= "46",
-		["NUMBER_4"]		= "15",
-		["NUMBER_5"]		= "15",
-		["NUMBER_6"]		= "45",
-		["NUMBER_7"]		= "00",
-		["NUMBER_8"]		= "1F",
-		["NUMBER_9"]		= "1B",
-		["NUMBER_0"]		= "1E"
+		["LEFT"]		= "57",
+		["RIGHT"]		= "53",
+		["ENTER"]		= "04",
+		["NUMBER_1"]	= "1D",
+		["NUMBER_2"]	= "1C",
+		["NUMBER_3"]	= "46",
+		["NUMBER_4"]	= "15",
+		["NUMBER_5"]	= "15",
+		["NUMBER_6"]	= "45",
+		["NUMBER_7"]	= "00",
+		["NUMBER_8"]	= "1F",
+		["NUMBER_9"]	= "1B",
+		["NUMBER_0"]	= "1E"
 }
 CMDS_SERIAL = {	 
 		--Power
-		["ON"]             = "F5 88 00 FE FE 01",
-		["OFF"]            = "F5 88 00 FE FE 00",
+		["ON"]		= "F5 88 00 FE FE 01",
+		["OFF"]		= "F5 88 00 FE FE 00",
 		
 		--Input Toggle
-		["DP"]	=	"F5 88 00 FE 01 01",
-		["HDMI"] =		"F5 88 00 FE 01 02"
+		["DP"]		= "F5 88 00 FE 01 01",
+		["HDMI"] 		= "F5 88 00 FE 01 02"
 }
 
 function ProcessCommand(strCommand)
@@ -46,26 +46,21 @@ function ProcessCommand(strCommand)
 		-- return concated keycode
 		command = string.format(OSD_MENU, SET_ID, KEY_CODES[strCommand]) 
 		C4:DebugLog("Key code found: " .. command)
-		print("Key code found: " .. command)
 	elseif CMDS_SERIAL[strCommand] ~= nil then
 		-- return command from cmds
 		command = CMDS_SERIAL[strCommand]
 		C4:DebugLog("Serial command found: " .. command)
-		print("Serial command found: " .. command)
 	else
 		-- Command not supported
 	   	C4:DebugLog(strCommand .. " command not found for binding id: " .. SERIAL_BINDING)
-	   	print(strCommand .. " command not found for binding id: " .. SERIAL_BINDING)
 	end
     return command
 end
 
 function OnPropertyChanged(strName)
-    print(strName)
-    print (Properties[strName])
     SET_ID = Properties[strName]
-    print (SET_ID)
 end
+
 function ReceivedFromProxy(idBinding, strCommand, tParams)
 	local command = ProcessCommand(strCommand)
 	C4:SendToSerial(SERIAL_BINDING, tohex(command))
@@ -73,8 +68,8 @@ function ReceivedFromProxy(idBinding, strCommand, tParams)
 end
 
 function ReceivedFromSerial(idBinding, strData)
-    local serialData = hexdump(strData)
-    
-    C4:DebugLog("Received from serial device: " .. serialData)
-    print("Received from serial device: " .. serialData)
+    -- local serialData = hexdump(strData)
+    -- TODO Send this back to the proxy
+    hexdump(strData)
+    C4:DebugLog("Received from serial device: " .. strData)
 end
